6ebb30cb24fa0fdc90dfc31545aed174dd65f76c
Hong Hi <1175805344@qq.com>
2018-11-11 17:43:33 +0800

    first commit

---
+++ src-01-base/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h2>第一个Vue组件项目</h2>
+      <img src="assets/logo.png" alt="logo"/>
+      <HelloWorld/>
+    </div>
+</template>
+
+<script scoped>
+  import HelloWorld from './components/HelloWorld.vue'
+    export default {
+      components:{
+        HelloWorld
+      }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src-01-base/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h2>{{msg}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+      data(){
+        return{
+          msg: '这是一个图标'
+        }
+      }
+    }
+</script>
+
+<style>
+h2{
+  color: red;
+}
+</style>
